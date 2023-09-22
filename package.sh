a2402d1863d5383cbd788a531114f8a5ebbc9025
Nicholas Smith <nicholas@nbembedded.com>
2023-09-22 20:31:44 +1000

    add package script

---
+++ package.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm opencart-3.0.3-8-linux-amd64-debian-11.tar.gz
+rm opencart-3.0.3-8-linux-amd64-debian-11.tar.gz.sha256
+tar -czvf opencart-3.0.3-8-linux-amd64-debian-11.tar.gz opencart-3.0.3-8-linux-amd64-debian-11
+sum="$(shasum -a 256 opencart-3.0.3-8-linux-amd64-debian-11.tar.gz)"
+echo "$sum" > opencart-3.0.3-8-linux-amd64-debian-11.tar.gz.sha256
