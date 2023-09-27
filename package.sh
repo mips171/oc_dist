cd3aa950b512b509383d24f1a64d67f45b7272d3
Nicholas Smith <nicholas@nbembedded.com>
2023-09-27 18:15:41 +1000

    automate package
    Signed-off-by: Nicholas Smith <nicholas@nbembedded.com>

--- package.sh
+++ package.sh
@@ -5,3 +5,7 @@ rm opencart-3.0.3-8-linux-amd64-debian-11.tar.gz.sha256
 tar -czvf opencart-3.0.3-8-linux-amd64-debian-11.tar.gz opencart-3.0.3-8-linux-amd64-debian-11
 sum="$(shasum -a 256 opencart-3.0.3-8-linux-amd64-debian-11.tar.gz)"
 echo "$sum" > opencart-3.0.3-8-linux-amd64-debian-11.tar.gz.sha256
+
+git add .
+git commit -m "update opencart-3.0.3-8-linux-amd64-debian-11.tar.gz"
+git push
