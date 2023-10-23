c5c776141b623c5c15e48fc2e33dde9111e4fefa
Nicholas Smith <nicholas@nbembedded.com>
2023-10-24 09:29:25 +1000

    update opencart-3.0.3-8-linux-amd64-debian-11.tar.gz

--- opencart-3.0.3-8-linux-amd64-debian-11/files/opencart/catalog/model/extension/shipping/auspost.php
+++ opencart-3.0.3-8-linux-amd64-debian-11/files/opencart/catalog/model/extension/shipping/auspost.php
@@ -132,7 +132,13 @@ class ModelExtensionShippingAusPost extends Model {
 				if ($response) {
 					$response_info = array();
 
-					$response_parts = json_decode($response, true);
+					if (json_last_error() === JSON_ERROR_NONE) {
+						// It's a valid JSON
+						$response_parts = json_decode($response, true);
+					} else {
+						// Handle invalid JSON error
+						echo "Invalid JSON received: " . $response;
+					}
 
 					if (isset($response_parts['errors'])) {
 						$error = $response_parts['errors'][0]['message'];
