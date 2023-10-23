f20a98297e957056880339984b43cf56c30135e7
Nicholas Smith <nicholas@nbembedded.com>
2023-10-23 13:15:49 +1000

    update opencart-3.0.3-8-linux-amd64-debian-11.tar.gz

---
+++ opencart-3.0.3-8-linux-amd64-debian-11/files/opencart/admin/language/en-gb/extension/shipping/starshipit.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']      = 'Starshipit';
+
+// Text
+$_['text_extension']     = 'Extensions';
+$_['text_success']       = 'Success: You have modified Starshipit shipping!';
+$_['text_edit']          = 'Edit Starshipit Shipping';
+
+// Entry
+$_['entry_api']          = 'API Key';
+$_['entry_subscription'] = 'Subscription Key';
+$_['entry_postcode']     = 'Post Code';
+$_['entry_weight_class'] = 'Weight Class';
+$_['entry_tax_class']    = 'Tax Class';
+$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sort Order';
+
+// Help
+$_['help_weight_class']  = 'Set to kilograms.';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify Starshipit shipping!';
+$_['error_api']          = 'Error setting API key. Please check it here: https://app.starshipit.com/Members/Settings/Api2.aspx';
+$_['error_subscription'] = 'Error setting subscription key. Please check it here: https://app.starshipit.com/Members/Settings/Api2.aspx';
+$_['error_postcode']     = 'Post Code must be 4 digits!';
