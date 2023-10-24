d0cdc4198e8846b96658c2b4e5a911a6befa47d9
Nicholas Smith <nicholas@nbembedded.com>
2023-10-24 10:23:32 +1000

    update opencart-3.0.3-8-linux-amd64-debian-11.tar.gz

--- opencart-3.0.3-8-linux-amd64-debian-11/files/opencart/catalog/model/extension/shipping/auspost.php
+++ opencart-3.0.3-8-linux-amd64-debian-11/files/opencart/catalog/model/extension/shipping/auspost.php
@@ -23,141 +23,145 @@ class ModelExtensionShippingAusPost extends Model {
 
 		$api_key = $this->config->get('shipping_auspost_api');
 		$api_password = $this->config->get('shipping_auspost_password');
-		$api_account_no = $this->config->get('shipping_auspost_account_number');
+		$account_numbers = explode(',', $this->config->get('shipping_auspost_account_number'));
 
-		$quote_data = array();
+		foreach ($account_numbers as $account_no) {
+			$api_account_no = trim($account_no);
 
-		if ($status) {
-			$weight = $this->weight->convert($this->cart->getWeight(), $this->config->get('config_weight_class_id'), $this->config->get('shipping_auspost_weight_class_id'));
+			$quote_data = array();
 
-			$length = 0;
-			$width = 0;
-			$height = 0;
+			if ($status) {
+				$weight = $this->weight->convert($this->cart->getWeight(), $this->config->get('config_weight_class_id'), $this->config->get('shipping_auspost_weight_class_id'));
 
-			if ($address['iso_code_2'] == 'AU') {
-				$max_length = 0;
+				$length = 0;
+				$width = 0;
+				$height = 0;
 
-				foreach ($this->cart->getProducts() as $product) {
-					$fromUnit = $this->getLengthUnitByID($product['length_class_id']);
-					$toUnit = 'cm'; // Since the AusPost API always wants centimeters
+				if ($address['iso_code_2'] == 'AU') {
+					$max_length = 0;
 
-					$converted_length = $this->convertLength($product['length'], $fromUnit, $toUnit);
-					$converted_width = $this->convertLength($product['width'], $fromUnit, $toUnit);
-					$converted_height = $this->convertLength($product['height'], $fromUnit, $toUnit);
+					foreach ($this->cart->getProducts() as $product) {
+						$fromUnit = $this->getLengthUnitByID($product['length_class_id']);
+						$toUnit = 'cm'; // Since the AusPost API always wants centimeters
 
-					if ($converted_length > $max_length) {
-						$max_length = $converted_length;
+						$converted_length = $this->convertLength($product['length'], $fromUnit, $toUnit);
+						$converted_width = $this->convertLength($product['width'], $fromUnit, $toUnit);
+						$converted_height = $this->convertLength($product['height'], $fromUnit, $toUnit);
+
+						if ($converted_length > $max_length) {
+							$max_length = $converted_length;
+						}
+
+						if ($converted_height > $height) {
+							$height = $converted_height;
+						}
+
+						if ($converted_width > $width) {
+							$width = $converted_width;
+						}
 					}
 
-					if ($converted_height > $height) {
-						$height = $converted_height;
+					$length = $max_length; // Assign the maximum length after iterating through all products
+
+					$AUSPOST_API_BASE="https://digitalapi.auspost.com.au/";
+					$AUSPOST_API_TEST="test/";
+					$AUSPOST_API_SHIPPING_ENDPOINT="shipping/v1/prices/shipments";
+
+					$formatted_weight = (float)number_format($weight, 1, '.', '');
+					if ($formatted_weight == 0.0) {
+						// fallback weight
+						$formatted_weight = 0.5;
 					}
 
-					if ($converted_width > $width) {
-						$width = $converted_width;
-					}
-				}
-
-				$length = $max_length; // Assign the maximum length after iterating through all products
-
-				$AUSPOST_API_BASE="https://digitalapi.auspost.com.au/";
-				$AUSPOST_API_TEST="test/";
-				$AUSPOST_API_SHIPPING_ENDPOINT="shipping/v1/prices/shipments";
-
-				$formatted_weight = (float)number_format($weight, 1, '.', '');
-				if ($formatted_weight == 0.0) {
-					// fallback weight
-					$formatted_weight = 0.5;
-				}
-
-				$shipment_data = array(
-					'shipments' => array(
-						array(
-							'from' => array(
-								'suburb' => $this->config->get('shipping_auspost_suburb'),
-								'state'  => $this->config->get('shipping_auspost_state'),
-								'postcode' => $this->config->get('shipping_auspost_postcode')
-							),
-							'to' => array(
-								'suburb' => $address['city'],
-								'state'  => $this->getAbbreviatedState($address['zone']),
-								'postcode' => $address['postcode']
-							),
-							'items' => array(
-								array(
-									'length' => $length,
-									'height' => $height,
-									'width' => $width,
-									'weight' => (string)$formatted_weight, // using the formatted weight
-									'packaging_type' => $this->config->get('shipping_auspost_packaging_type')
+					$shipment_data = array(
+						'shipments' => array(
+							array(
+								'from' => array(
+									'suburb' => $this->config->get('shipping_auspost_suburb'),
+									'state'  => $this->config->get('shipping_auspost_state'),
+									'postcode' => $this->config->get('shipping_auspost_postcode')
+								),
+								'to' => array(
+									'suburb' => $address['city'],
+									'state'  => $this->getAbbreviatedState($address['zone']),
+									'postcode' => $address['postcode']
+								),
+								'items' => array(
+									array(
+										'length' => $length,
+										'height' => $height,
+										'width' => $width,
+										'weight' => (string)$formatted_weight, // using the formatted weight
+										'packaging_type' => $this->config->get('shipping_auspost_packaging_type')
+									)
 								)
 							)
 						)
-					)
-				);
+					);
 
-				$curl = curl_init();
+					$curl = curl_init();
 
-				$credentials = $api_key . ':' . $api_password;
-				$base64Credentials = base64_encode($credentials);
+					$credentials = $api_key . ':' . $api_password;
+					$base64Credentials = base64_encode($credentials);
 
-				curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-					'Content-Type: application/json',
-					'account-number: ' . $api_account_no,
-					'Authorization: Basic ' . $base64Credentials
-					// Add other headers if required
-				));
+					curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+						'Content-Type: application/json',
+						'account-number: ' . $api_account_no,
+						'Authorization: Basic ' . $base64Credentials
+						// Add other headers if required
+					));
 
-				$full_url = $AUSPOST_API_BASE . $AUSPOST_API_SHIPPING_ENDPOINT;
+					$full_url = $AUSPOST_API_BASE . $AUSPOST_API_SHIPPING_ENDPOINT;
 
-				if ($this->config->get('shipping_auspost_testmode')) {
-					$full_url = $AUSPOST_API_BASE . $AUSPOST_API_TEST . $AUSPOST_API_SHIPPING_ENDPOINT;
-				}
-
-				// Capture and log the request details
-				$logMessage = "Endpoint URL: " . $full_url . "\n";
-				$logMessage .= "Payload: " . json_encode($shipment_data) . "\n";
-				file_put_contents(DIR_LOGS . 'auspost_request.log', $logMessage, FILE_APPEND);
-
-
-				curl_setopt($curl, CURLOPT_URL, $full_url);
-				curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-				curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
-				curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($shipment_data));
-
-				$response = curl_exec($curl);
-
-				curl_close($curl);
-
-				if ($response) {
-					$response_info = array();
-
-					if (json_last_error() === JSON_ERROR_NONE) {
-						// It's a valid JSON
-						$response_parts = json_decode($response, true);
-					} else {
-						// Handle invalid JSON error
-						$error = "Invalid JSON received" . $response;
-						file_put_contents(DIR_LOGS . 'auspost_request.log', $response, FILE_APPEND);
+					if ($this->config->get('shipping_auspost_testmode')) {
+						$full_url = $AUSPOST_API_BASE . $AUSPOST_API_TEST . $AUSPOST_API_SHIPPING_ENDPOINT;
 					}
 
-					if (isset($response_parts['errors'])) {
-						$error = $response_parts['errors'][0]['message'];
-						file_put_contents(DIR_LOGS . 'auspost_request.log', $error, FILE_APPEND);
-					} else {
-						$shipments = $response_parts['shipments'];
+					// Capture and log the request details
+					$logMessage = "Endpoint URL: " . $full_url . "\n";
+					$logMessage .= "Payload: " . json_encode($shipment_data) . "\n";
+					file_put_contents(DIR_LOGS . 'auspost_request.log', $logMessage, FILE_APPEND);
 
-						foreach ($shipments as $shipment) {
-							$service_name = $shipment['items'][0]['product_id'];
-							$shipping_cost = $shipment['shipment_summary']['total_cost'];
 
-							$quote_data[$service_name] = array(
-								'code'         => 'auspost.' .  $service_name,
-								'title'        => $service_name,
-								'cost'         => $shipping_cost,
-								'tax_class_id' => $this->config->get('shipping_auspost_tax_class_id'),
-								'text'         => $this->currency->format($this->tax->calculate($this->currency->convert($shipping_cost, 'AUD', $this->session->data['currency']), $this->config->get('shipping_auspost_tax_class_id'), $this->config->get('config_tax')), $this->session->data['currency'], 1.0000000)
-							);
+					curl_setopt($curl, CURLOPT_URL, $full_url);
+					curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+					curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+					curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($shipment_data));
+
+					$response = curl_exec($curl);
+
+					curl_close($curl);
+
+					if ($response) {
+						$response_info = array();
+
+						if (json_last_error() === JSON_ERROR_NONE) {
+							// It's a valid JSON
+							$response_parts = json_decode($response, true);
+						} else {
+							// Handle invalid JSON error
+							$error = "Invalid JSON received" . $response;
+							file_put_contents(DIR_LOGS . 'auspost_request.log', $response, FILE_APPEND);
+						}
+
+						if (isset($response_parts['errors'])) {
+							$error = $response_parts['errors'][0]['message'];
+							file_put_contents(DIR_LOGS . 'auspost_request.log', $error, FILE_APPEND);
+						} else {
+							$shipments = $response_parts['shipments'];
+
+							foreach ($shipments as $shipment) {
+								$service_name = $shipment['items'][0]['product_id'];
+								$shipping_cost = $shipment['shipment_summary']['total_cost'];
+
+								$quote_data[$service_name] = array(
+									'code'         => 'auspost.' .  $service_name,
+									'title'        => $service_name,
+									'cost'         => $shipping_cost,
+									'tax_class_id' => $this->config->get('shipping_auspost_tax_class_id'),
+									'text'         => $this->currency->format($this->tax->calculate($this->currency->convert($shipping_cost, 'AUD', $this->session->data['currency']), $this->config->get('shipping_auspost_tax_class_id'), $this->config->get('config_tax')), $this->session->data['currency'], 1.0000000)
+								);
+							}
 						}
 					}
 				}
