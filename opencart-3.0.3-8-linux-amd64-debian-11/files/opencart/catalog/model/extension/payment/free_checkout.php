48cd3355b0d3cd9ba1b8a402ae614586b06f5404
Nicholas Smith <nicholas@nbembedded.com>
2023-10-27 15:44:27 +1000

    update opencart-3.0.3-8-linux-amd64-debian-11.tar.gz

--- opencart-3.0.3-8-linux-amd64-debian-11/files/opencart/catalog/model/extension/payment/free_checkout.php
+++ opencart-3.0.3-8-linux-amd64-debian-11/files/opencart/catalog/model/extension/payment/free_checkout.php
@@ -1,25 +1,85 @@
 <?php
 class ModelExtensionPaymentFreeCheckout extends Model {
-	public function getMethod($address, $total) {
-		$this->load->language('extension/payment/free_checkout');
+	private $customerData = [];
 
-		if ($total <= 0.00) {
-			$status = true;
-		} else {
-			$status = false;
+    public function getMethod($address, $total) {
+        $this->load->language('extension/payment/free_checkout');
+
+		// Check if logged in customer is on credit hold
+		$onCreditHold = $this->isCustomerOnCreditHold();
+
+		if ($onCreditHold) {
+			return array();
 		}
 
-		$method_data = array();
+        // Check if logged in customer belongs to the "trade" group
+        $isTradeGroup = $this->isCustomerInTradeGroup();
 
-		if ($status) {
-			$method_data = array(
-				'code'       => 'free_checkout',
-				'title'      => $this->language->get('text_title'),
-				'terms'      => '',
-				'sort_order' => $this->config->get('payment_free_checkout_sort_order')
-			);
+		if (!$isTradeGroup) {
+			return array();
 		}
 
-		return $method_data;
+        // Get customer credit balance
+        $creditBalance = $this->getCustomerCreditBalance();
+
+        // Check if total is less than or equal to zero or customer belongs to trade group and has sufficient credit
+        if ($creditBalance >= $total) {
+            $status = true;
+        } else {
+            $status = false;
+        }
+
+        $method_data = array();
+
+        if ($status) {
+            $method_data = array(
+                'code'       => 'free_checkout',
+                'title'      => $this->language->get('text_title'),
+                'terms'      => '',
+                'sort_order' => $this->config->get('payment_free_checkout_sort_order')
+            );
+        }
+
+        return $method_data;
+    }
+
+    private function isCustomerInTradeGroup() {
+        $tradeGroupId = 2;
+
+        if (!$this->customer->isLogged()) {
+            return false;
+        }
+
+        $customerGroupId = $this->customer->getGroupId();
+        return $customerGroupId == $tradeGroupId;
+    }
+
+	private function getCustomerData() {
+		if (!empty($this->customerData)) {
+			return $this->customerData;
+		}
+
+		if (!$this->customer->isLogged()) {
+			return [];
+		}
+
+		$query = $this->db->query("SELECT credit, credit_hold FROM " . DB_PREFIX . "customer WHERE customer_id = '" . (int)$this->customer->getId() . "'");
+
+		if ($query->num_rows) {
+			$this->customerData = $query->row;
+		}
+
+		return $this->customerData;
 	}
+
+    private function getCustomerCreditBalance() {
+        $customerData = $this->getCustomerData();
+        return isset($customerData['credit']) ? (float)$customerData['credit'] : 0;
+    }
+
+    private function isCustomerOnCreditHold() {
+        $customerData = $this->getCustomerData();
+        return isset($customerData['credit_hold']) ? (bool)$customerData['credit_hold'] : true;
+    }
+
 }
