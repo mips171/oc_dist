93596eaa28e4513071aaae129d44cae1c4ad9a5a
Nicholas Smith <nicholas@nbembedded.com>
2023-11-22 19:21:53 +1000

    update opencart-3.0.3-8-linux-amd64-debian-11.tar.gz

--- opencart-3.0.3-8-linux-amd64-debian-11/files/opencart/admin/model/customer/customer.php
+++ opencart-3.0.3-8-linux-amd64-debian-11/files/opencart/admin/model/customer/customer.php
@@ -131,7 +131,7 @@ class ModelCustomerCustomer extends Model
         }
 
         if ($implode) {
-            $sql .= " AND " . implode(" OR ", $implode);
+            $sql .= " AND " . implode(" AND ", $implode);
         }
 
         $sort_data = array(
@@ -249,41 +249,39 @@ class ModelCustomerCustomer extends Model
     {
         $sql = "SELECT COUNT(*) AS total FROM " . DB_PREFIX . "customer c";
 
-        $conditions = array();
+        $implode = array();
 
-        // Refactor conditions to make them clearer and case-insensitive
         if (!empty($data['filter_name'])) {
-            $conditions[] = "CONCAT(LOWER(firstname), ' ', LOWER(lastname)) LIKE '%" . $this->db->escape(strtolower($data['filter_name'])) . "%'";
+            $lowercasedName = strtolower($this->db->escape($data['filter_name']));
+            $implode[] = "CONCAT(LOWER(c.firstname), ' ', LOWER(c.lastname)) LIKE '%" . $lowercasedName . "%'";
         }
 
         if (!empty($data['filter_email'])) {
-            $conditions[] = "LOWER(email) LIKE '" . $this->db->escape(strtolower($data['filter_email'])) . "%'";
+            $implode[] = "LOWER(c.email) LIKE '%" . $this->db->escape(strtolower($data['filter_email'])) . "%'";
         }
 
-        // Other conditions remain the same
         if (isset($data['filter_newsletter']) && !is_null($data['filter_newsletter'])) {
-            $conditions[] = "newsletter = '" . (int) $data['filter_newsletter'] . "'";
+            $implode[] = "newsletter = '" . (int) $data['filter_newsletter'] . "'";
         }
 
         if (!empty($data['filter_customer_group_id'])) {
-            $conditions[] = "customer_group_id = '" . (int) $data['filter_customer_group_id'] . "'";
+            $implode[] = "customer_group_id = '" . (int) $data['filter_customer_group_id'] . "'";
         }
 
         if (!empty($data['filter_ip'])) {
-            $conditions[] = "customer_id IN (SELECT customer_id FROM " . DB_PREFIX . "customer_ip WHERE ip = '" . $this->db->escape($data['filter_ip']) . "')";
+            $implode[] = "customer_id IN (SELECT customer_id FROM " . DB_PREFIX . "customer_ip WHERE ip = '" . $this->db->escape($data['filter_ip']) . "')";
         }
 
         if (isset($data['filter_status']) && $data['filter_status'] !== '') {
-            $conditions[] = "status = '" . (int) $data['filter_status'] . "'";
+            $implode[] = "status = '" . (int) $data['filter_status'] . "'";
         }
 
         if (!empty($data['filter_date_added'])) {
-            $conditions[] = "DATE(date_added) = DATE('" . $this->db->escape($data['filter_date_added']) . "')";
+            $implode[] = "DATE(date_added) = DATE('" . $this->db->escape($data['filter_date_added']) . "')";
         }
 
-        // Append conditions to the SQL query
-        if (!empty($conditions)) {
-            $sql .= " WHERE " . implode(" OR ", $conditions);
+        if ($implode) {
+            $sql .= " WHERE " . implode(" AND ", $implode);
         }
 
         $query = $this->db->query($sql);
