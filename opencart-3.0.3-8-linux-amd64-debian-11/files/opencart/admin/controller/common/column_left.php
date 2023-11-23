164bd08095dd38950a1ee88446df181a6a545fce
Nicholas Smith <nicholas@nbembedded.com>
2023-11-23 10:47:31 +1000

    update opencart-3.0.3-8-linux-amd64-debian-11.tar.gz

--- opencart-3.0.3-8-linux-amd64-debian-11/files/opencart/admin/controller/common/column_left.php
+++ opencart-3.0.3-8-linux-amd64-debian-11/files/opencart/admin/controller/common/column_left.php
@@ -130,18 +130,18 @@ class ControllerCommonColumnLeft extends Controller
             // Catalog
             $catalog = array();
 
-            if ($this->user->hasPermission('access', 'catalog/category')) {
-                $catalog[] = array(
-                    'name' => $this->language->get('text_category'),
-                    'href' => $this->url->link('catalog/category', 'user_token=' . $this->session->data['user_token'], true),
-                    'children' => array()
-                );
-            }
-
             if ($this->user->hasPermission('access', 'catalog/product')) {
                 $catalog[] = array(
                     'name' => $this->language->get('text_product'),
                     'href' => $this->url->link('catalog/product', 'user_token=' . $this->session->data['user_token'], true),
+                    'children' => array()
+                );
+            }
+
+            if ($this->user->hasPermission('access', 'catalog/category')) {
+                $catalog[] = array(
+                    'name' => $this->language->get('text_category'),
+                    'href' => $this->url->link('catalog/category', 'user_token=' . $this->session->data['user_token'], true),
                     'children' => array()
                 );
             }
